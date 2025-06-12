b8c43fd207ae1fc4cff605011592ff2ee66f7046
LazyLEE1008 <shlee6630@gmail.com>
2025-06-12 22:12:28 +0900

    lectur06_tree

---
+++ lecture06_tree/lecture06_tree/lecture06_tree.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+int main()
+{
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    pq.push(4);
+    pq.push(8);
+    pq.push(51);
+    pq.push(16);
+    pq.push(50);
+
+    pq.pop();
+    
+    
+
+    cout << "hello" << endl;
+
+
+
+
+}
+
